e9b19c05efd9ee65332a77df430937b21aa24266
Yuri Quezada <yuri_quezada@hotmail.com>
2019-10-15 17:38:02 -0500

    Cambiar class por ClassName

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ function App() {
   return (
     <div className="App">
       <p>LATAM AIRLINES</p>
-      <ul class="nav nav-tabs" id="myTab" role="tablist">
-        <li class="nav-item">
-          <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+      <ul className="nav nav-tabs" id="myTab" role="tablist">
+        <li className="nav-item">
+          <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+        <li className="nav-item">
+          <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+        <li className="nav-item">
+          <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
         </li>
       </ul>
-      <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">lorem</div>
-        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">ipsum</div>
-        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">turis</div>
+      <div className="tab-content" id="myTabContent">
+        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">lorem</div>
+        <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">ipsum</div>
+        <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">turis</div>
       </div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
