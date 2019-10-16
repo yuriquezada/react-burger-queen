7cc64b762a99ced71c85d0caa3f6a8b9fda1e809
Yuri Quezada <yuri_quezada@hotmail.com>
2019-10-16 17:08:37 -0500

    Tablas de menú (Lista de Productos) implementada

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-
-// function Saludo(){
-//   return(
-//     <div>Holaaaaaaaaaaaaaa</div>
-//   );
-// }
+import ProductList from './product-list/Product-list';
 
 function App() {
   return (
     <div className="App">
       <p>LATAM AIRLINES</p>
-      <ul className="nav nav-tabs" id="myTab" role="tablist">
-        <li className="nav-item">
-          <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-        </li>
-      </ul>
-      <div className="tab-content" id="myTabContent">
-        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">lorem</div>
-        <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">ipsum</div>
-        <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">turis</div>
+      <div className="row">
+        <ProductList/>
+        <div className="col-sm">
+          One of three columns
+        </div>
       </div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p><Saludo/></p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React ahora mismo
-        </a>
-      </header> */}
     </div>
   );
 }
---
+++ src/product-list/Product-list.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+
+function ProductList() {
+  return (
+    <div className="col-sm">
+      <ul className="nav nav-tabs" id="myTab" role="tablist">
+        <li className="nav-item">
+          <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+        </li>
+      </ul>
+      <div className="tab-content" id="myTabContent">
+        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+          <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <li className="nav-item">
+              <a className="nav-link active" id="d_Sandwich-tab" data-toggle="tab" href="#d_Sandwich" role="tab" aria-controls="d_Sandwich" aria-selected="true">Sandwich</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" id="d_Drink-tab" data-toggle="tab" href="#d_Drink" role="tab" aria-controls="d_Drink" aria-selected="false">Drink</a>
+            </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="d_Sandwich" role="tabpanel" aria-labelledby="d_Sandwich-tab">Lista de sandwiches</div>
+            <div class="tab-pane fade" id="d_Drink" role="tabpanel" aria-labelledby="d_Drink-tab">Lista de bebidas</div>
+          </div>
+        </div>
+
+        <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+          <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <li className="nav-item">
+              <a className="nav-link active" id="a_Burger-tab" data-toggle="tab" href="#a_Burger" role="tab" aria-controls="a_Burger" aria-selected="true">Hamburguesa</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" id="a_Accompaniment-tab" data-toggle="tab" href="#a_Accompaniment" role="tab" aria-controls="a_Accompaniment" aria-selected="false">Acompañamiento</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" id="a_Drink-tab" data-toggle="tab" href="#a_Drink" role="tab" aria-controls="a_Drink" aria-selected="false">Drink</a>
+            </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="a_Burger" role="tabpanel" aria-labelledby="a_Burger-tab">Lista de hamburguesas</div>
+            <div class="tab-pane fade" id="a_Accompaniment" role="tabpanel" aria-labelledby="a_Accompaniment-tab">Lista de acompañamientos</div>
+            <div class="tab-pane fade" id="a_Drink" role="tabpanel" aria-labelledby="a_Drink-tab">Lista de bebidas</div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
