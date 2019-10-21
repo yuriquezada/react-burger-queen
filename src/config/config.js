4ef2be2ceb4b772b68de6f5e76f84a9cfe791d86
Yuri Quezada <yuri_quezada@hotmail.com>
2019-10-21 17:58:03 -0500

    Inicializando firebase en App.js

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCtgymKSWV62YN8zimGsMlfDc0mruTi2qM",
+  authDomain: "burguer-queen-234c8.firebaseapp.com",
+  databaseURL: "https://burguer-queen-234c8.firebaseio.com",
+  projectId: "burguer-queen-234c8",
+  storageBucket: "burguer-queen-234c8.appspot.com",
+  messagingSenderId: "41829269712",
+  appId: "1:41829269712:web:c3ddee7e215289bcd31ed5"
+};
