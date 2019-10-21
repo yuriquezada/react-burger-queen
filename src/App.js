81e3cb191ed00d5f95bbe5aa3b35881102a4b39e
Yuri Quezada <yuri_quezada@hotmail.com>
2019-10-21 17:34:55 -0500

    Inicializando firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 import './App.css';
 import ProductList from './product-list/Product-list';
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCtgymKSWV62YN8zimGsMlfDc0mruTi2qM",
+  authDomain: "burguer-queen-234c8.firebaseapp.com",
+  databaseURL: "https://burguer-queen-234c8.firebaseio.com",
+  projectId: "burguer-queen-234c8",
+  storageBucket: "burguer-queen-234c8.appspot.com",
+  messagingSenderId: "41829269712",
+  appId: "1:41829269712:web:c3ddee7e215289bcd31ed5"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
