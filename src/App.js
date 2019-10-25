57c51855ac168be54e05343433233c31ff2a711c
SalaLectura <SalaLectura@ISILACADSI.PE>
2019-10-24 20:25:20 -0500

    Probando cuenta

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-import * as firebase from "firebase/app";
-import {firebaseConfig} from './config/config.js';
-// Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+
 
 
 import './App.css';
 import ProductList from './product-list/Product-list';
 
-firebase.initializeApp(firebaseConfig);
+
 
 
 function App() {
--- src/config/firestore-config.js
+++ src/config/firestore-config.js
@@ -1,5 +1,13 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+// import {firebaseConfig} from './config/firestore-config.js';
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 // Your web app's Firebase configuration
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCtgymKSWV62YN8zimGsMlfDc0mruTi2qM",
   authDomain: "burguer-queen-234c8.firebaseapp.com",
   databaseURL: "https://burguer-queen-234c8.firebaseio.com",
@@ -8,3 +16,7 @@ export const firebaseConfig = {
   messagingSenderId: "41829269712",
   appId: "1:41829269712:web:c3ddee7e215289bcd31ed5"
 };
+
+//probando cuenta
+
+firebase.initializeApp(firebaseConfig);
