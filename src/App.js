ba41b72f5a241d78303f7f7decdf28a9e1793639
Yuri Quezada <yuri_quezada@hotmail.com>
2019-10-29 15:51:10 -0500

    Almacena valores de data en un párrafo de prueba

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 import ProductList from './components/product-list/Product-list';
 // import Parrafo from './components/Parrafo';
 import db from './config/firebase'
 
-db.collection('/menu/').get()
-  .then((snapshot) => {
-    const data = [];
-    snapshot.forEach((doc) => {
-      data.push(doc.id);
 
-    });
-    console.log(data, 'este es el nuevo arreglo')
-  })
-  .catch((err) => {
-    console.log('Error getting documents', err);
-  });
+/**------------------------------*/
+const useMessage = () => {
+  const [messages, setMessages] = useState(null);
+
+  useEffect(() => {
+    listenForMessages();
+  }, []);
+
+  /*---------------------------**/
+
+  const listenForMessages = () => {
+    db.collection('/menu/').get()
+      .then((snapshot) => {
+        const data = [];
+        snapshot.forEach((doc) => {
+          data.push(doc.id);
+
+        });
+        setMessages(data);
+        console.log(data, 'este es el nuevo arreglo')
+      })
+      .catch((err) => {
+        console.log('Error getting documents', err);
+      });
+  }
+
+  return messages;
+}
 
 function App() {
+  const messages = useMessage()
+  // console.log(messages.length)
   return (
     <div className="App">
       <p>LATAM AIRLINES</p>
@@ -29,7 +48,7 @@ function App() {
         </div>
       </div>
       <div className="row">
-        
+        <p>{messages}</p>
       </div>
     </div>
   );
