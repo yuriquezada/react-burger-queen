349c85b6200986cb15b4915d72d31308752b2a80
Yuri Quezada <yuri_quezada@hotmail.com>
2019-10-29 17:32:44 -0500

    Probando en otro componente mostrar data

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import './App.css';
 import ProductList from './components/product-list/Product-list';
-// import Parrafo from './components/Parrafo';
-import db from './config/firebase'
+import Parrafo from './components/Parrafo';
+// import db from './config/firebase'
 
 
-/**------------------------------*/
-const useMessage = () => {
-  const [messages, setMessages] = useState(null);
 
-  useEffect(() => {
-    listenForMessages();
-  }, []);
-
-  /*---------------------------**/
-
-  const listenForMessages = () => {
-    db.collection('/menu/').get()
-      .then((snapshot) => {
-        const data = [];
-        snapshot.forEach((doc) => {
-          data.push(doc.id);
-
-        });
-        setMessages(data);
-        console.log(data, 'este es el nuevo arreglo')
-      })
-      .catch((err) => {
-        console.log('Error getting documents', err);
-      });
-  }
-
-  return messages;
-}
-
-function App() {
-  const messages = useMessage()
-  // console.log(messages.length)
-  return (
+const App = ()=>{
+    return (
     <div className="App">
       <p>LATAM AIRLINES</p>
       <div className="row">
@@ -47,14 +17,9 @@ function App() {
           One of three columns
         </div>
       </div>
-      <div className="row">
-        <p>{messages}</p>
-      </div>
+      <Parrafo/>
     </div>
   );
 }
-
-
-
 
 export default App;
--- src/components/Parrafo.js
+++ src/components/Parrafo.js
@@ -1,18 +1,43 @@
-// import React from 'react';
-// import db from '../config/firebase'
-
-// db.collection('/menu/').get()
-//   .then((snapshot) => {
-//     const data = [];
-//     snapshot.forEach((doc) => {
-//       data.push(doc.id);
-//       console.log(data)
-//     });
-//   })
-//   .catch((err) => {
-//     console.log('Error getting documents', err);
-//   });
+import React, { useState, useEffect } from 'react';
+import db from '../config/firebase'
 
 
+/**------------------------------*/
+const useMessage = () => {
+  const [messages, setMessages] = useState(null);
 
-// export default Parrafo;
+  useEffect(() => {
+    listenForMessages();
+  }, []);
+
+  /*---------------------------**/
+
+  const listenForMessages = () => {
+    db.collection('/menu/').get()
+      .then((snapshot) => {
+        const data = [];
+        snapshot.forEach((doc) => {
+          data.push(doc.id);
+
+        });
+        setMessages(data);
+        console.log(data, 'este es el nuevo arreglo')
+      })
+      .catch((err) => {
+        console.log('Error getting documents', err);
+      });
+  }
+
+  return messages;
+}
+
+const Parrafo = () => {
+  const messages = useMessage();
+  return (
+    <div className="row nuevoparrafito">
+      <p>{messages}</p>
+    </div>
+  )
+}
+
+export default Parrafo;
