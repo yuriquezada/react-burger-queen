8bd389eed37db38f9483f89cbc44cbd52261dc79
Yuri Quezada <yuri_quezada@hotmail.com>
2019-10-31 15:06:59 -0500

    Cambia de get a onSnapshot

--- src/functions/useMessage.js
+++ src/functions/useMessage.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import db from '../config/firebase'
 
-
 const useMessage = () => {
   const [messages, setMessages] = useState([]);
 
@@ -10,21 +9,57 @@ const useMessage = () => {
   }, []);
 
   const listenForMessages = () => {
-    db.collection('menu').get()
-      .then((snapshot) => {
+    db.collection('menu')
+      .onSnapshot((snapshot) => {
         const data = [];
         snapshot.forEach((doc) => {
           data.push(doc.id);
         });
         setMessages(data);
-        console.log(data, 'este es el nuevo arreglo')
-      })
-      .catch((err) => {
-        console.log('Error getting documents', err);
-      });
+        // console.log(data, 'este es el nuevo arreglo')
+      }, (error) => console.error(error))
   }
   console.log(messages, 'no sé que es')
   return messages;
 }
 
 export default useMessage;
+
+// const listenForMessages = () => {
+//   firestore.collection('messages')
+//     .onSnapshot((snapshot) => {
+//       // Loop through the snapshot and collect
+//       // the necessary info we need. Then push
+//       // it into our array
+//       const allMessages = [];
+//       snapshot.forEach((doc) => allMessages.push(doc.data()));
+
+//       // Set the collected array as our state
+//       setMessages(allMessages);
+//     }, (error) => console.error(error));
+// };
+
+// const useMessage = () => {
+//   const [messages, setMessages] = useState([]);
+
+//   useEffect(() => {
+//     listenForMessages();
+//   }, []);
+
+//   const listenForMessages = () => {
+//     db.collection('menu').get()
+//       .then((snapshot) => {
+//         const data = [];
+//         snapshot.forEach((doc) => {
+//           data.push(doc.id);
+//         });
+//         setMessages(data);
+//         console.log(data, 'este es el nuevo arreglo')
+//       })
+//       .catch((err) => {
+//         console.log('Error getting documents', err);
+//       });
+//   }
+//   console.log(messages, 'no sé que es')
+//   return messages;
+// }
