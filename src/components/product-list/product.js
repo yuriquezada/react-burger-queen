e345d1c23c9c2087c3f92ca7b374f747cafe9dac
Yuri Quezada <yuri_quezada@hotmail.com>
2019-11-13 10:49:51 -0500

    Estilos de card prodcut

--- src/components/product-list/product.js
+++ src/components/product-list/product.js
@@ -6,16 +6,21 @@ const Product = (props) => {
   const messages = useMessage(props.path);
   console.log(messages, 'es el arreglo dentro de Product', messages.length);
   return (
+    <div className="row">{
       messages.map((message, index) =>
-        <div className="card" onClick={handleClick} key={index}>
-          <img src="..." className="card-img-top" alt="..."/>
+        <div className="col-sm-4">
+          <div className="card" onClick={handleClick} key={index}>
+            <img src={message.image} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{message.item}</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="https://github.com/" className="btn btn-primary">Go somewhere</a>
+              <p className="card-text">$ {message.price}</p>
+              <a href="https://github.com/" className="btn btn-primary">Add order</a>
             </div>
+          </div>
         </div>
-          )
-      );
-      };
+      )
+    }</div>
+  );
+};
 export default Product;
+
