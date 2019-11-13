74d803dd2752d4f8e85e7cdee4a7707a94b2cd8b
Yuri Quezada <yuri_quezada@hotmail.com>
2019-11-13 12:42:45 -0500

    Updates

---
+++ precache-manifest.9652235edeb5c793dee035e6c159602b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa9f660609225950ee95f0c8e18000f",
+    "url": "/react-burger-queen/index.html"
+  },
+  {
+    "revision": "084a8be1b493ced5ce1f",
+    "url": "/react-burger-queen/static/css/main.58d1d0f8.chunk.css"
+  },
+  {
+    "revision": "58942059107897100eec",
+    "url": "/react-burger-queen/static/js/2.422fd578.chunk.js"
+  },
+  {
+    "revision": "084a8be1b493ced5ce1f",
+    "url": "/react-burger-queen/static/js/main.1d6c9bdb.chunk.js"
+  },
+  {
+    "revision": "2882f727c070eea6fb18",
+    "url": "/react-burger-queen/static/js/runtime-main.793f3011.js"
+  }
+]);
