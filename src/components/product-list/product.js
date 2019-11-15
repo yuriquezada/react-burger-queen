67b40c5a44ca8370c120bb73d99bcbd67c69c4a8
yuriquezada <yuri_quezada@hotmail.com>
2019-11-14 21:25:14 -0500

    Añadiendo nuevas imágenes

--- src/components/product-list/product.js
+++ src/components/product-list/product.js
@@ -6,16 +6,33 @@ const Product = (props) => {
   const messages = useMessage(props.path);
   console.log(messages, 'es el arreglo dentro de Product', messages.length);
   return (
+    <div className="card-deck">{
       messages.map((message, index) =>
-        <div className="card" onClick={handleClick} key={index}>
-          <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{message.item}</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="https://github.com/" className="btn btn-primary">Go somewhere</a>
-            </div>
+        <div className="card col-lg-4 col-md-6" onClick={handleClick} key={index}>
+          <img src={message.image} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{message.item}</h5>
+          </div>
+          <div className="card-footer">
+            <small className="text-muted">Last updated 3 mins ago</small>
+            <a href="https://github.com/" className="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
-          )
-      );
-      };
+      )
+    }</div>
+
+  );
+};
 export default Product;
+
+// <div className="card" onClick={handleClick} key={index}>
+//   <img src="..." className="card-img-top" alt="..."/>
+//   <div className="card-body">
+//     <h5 className="card-title">{message.item}</h5>
+//     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//   </div>
+//   <div className="card-footer">
+//     <small className="text-muted">Last updated 3 mins ago</small>
+//     <a href="https://github.com/" className="btn btn-primary">Go somewhere</a>
+//   </div>
+// </div>
