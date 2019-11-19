65f2e6965feb7d9f0fc6a6a69e9a4b386240b612
Yuri Quezada <yuri_quezada@hotmail.com>
2019-11-19 17:51:09 -0500

    Crear archivo use-Order

---
+++ src/functions/use-order.js
@@ -0,0 +1,9 @@
+// import { useState} from 'react';
+
+// const useOrder = (nombre, precio) =>{
+//   const [name, setName] = useState({firstName: '', price: ''});
+
+//   setName({firstName: nombre, precio: precio})
+// }
+
+// export default useOrder;
